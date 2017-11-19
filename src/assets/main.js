da91a0ac24d23fcc5fa8137b9a90a13ee57aa14f
Meagan Blais <meaganblais@gmail.com>
2017-11-18 23:39:35 -0500

    adding setHiddenFields function

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,7 @@
 let answer = document.getElementById('answer');
+// limit attempt to 4 characters
 let attempt = document.getElementById('attempt');
+
 
 function guess() {
     let input = document.getElementById('user-guess');
@@ -7,3 +9,23 @@ function guess() {
 }
 
 //implement new functions here
+
+// function setHiddenFields (min, max) {
+//     min = Math.ceil(min);
+//     max = Math.floor(max);
+//     answer = Math.floor(Math.random() * (max - min)) + min;
+//     while (answer.length < 4) {
+
+//     }
+// }
+// setHiddenFields(0, 9999);
+
+function setHiddenFields () {
+    let random = Math.floor(Math.random());
+    while (random.toString().length <= 4) {
+        random = "0" + random.toString();
+    }
+    answer = random;
+    attempt = 0;
+    console.log(answer);
+}
